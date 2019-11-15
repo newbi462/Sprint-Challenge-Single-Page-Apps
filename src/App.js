6dc237fa905a17aa5ee5b13506c96865ab3c39d7
newbi462 <isthisemailfree@gmail.com>
2019-11-15 11:48:23 -0500

    Hook up the Welcome page(Home page) and a Characters page with React Router.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 
 import Header from "./components/Header.js";
 import CharacterCard from "./components/CharacterCard";
+import WelcomePage from "./components/WelcomePage";
 
 export default function App() {
   return (
     <main>
       <Header />
 
-      <CharacterCard />
+      <Route exact path="/" component={WelcomePage} />
+      <Route path="/charaters" component={CharacterCard} />
     </main>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import { Link } from 'react-router-dom';
+
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+
+      <div>
+        {/*}<a href="">Home</a>*/}
+        <Link to="/">Home</Link>
+      </div>
+      <div>
+        {/*<a href="">About</a>*/}
+        <Link to="/charaters">Charaters</Link>
+      </div>
     </header>
   );
 }
